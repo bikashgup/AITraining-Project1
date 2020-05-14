15e24284e37537d578cb03b6879723cdaabb7e4e
bikashgup <bikashgup@hotmail.com>
2020-05-14 17:31:54 +0545

    [AIT-1] Editing the data path

--- ml/config/data_paths.py
+++ ml/config/data_paths.py
@@ -1,1 +1,6 @@
-ISEAR_DATA_PATH = 'data/ISEAR.csv'
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
+
+
+ISEAR_DATA_PATH = BASE_DIR + '/ml/data/ISEAR.csv'
