4bf8b9f63355fcb3911155c8b5e7bd6317515d2c
bikashgup <bikashgup@hotmail.com>
2020-05-15 12:25:22 +0545

    Editted data_loading file in data_cleaning folder
    - created the emotion_id column
    - sorted the data according to emotion_id

--- ml/data_cleaning/data_loading.py
+++ ml/data_cleaning/data_loading.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ..config import data_paths as Dpath
 
 def loading_ISEAR_data(path):
     '''
@@ -22,5 +25,18 @@ def loading_ISEAR_data(path):
     data.drop(columns=[0], inplace=True)
     target_labels = data[1].value_counts().keys().tolist()
     print(target_labels)
+    data[1] = data[1].astype('category')
+    ## adding the column containing numerical representatipn of emotion in column 1
+    data[3] = data[1].cat.codes
+    ## sorting the dataframe according to product id
+    data = data.sort_values(3)
+    ## countplot for emotion column i.e column 1 in dataframe
+    fig, ax1 = plt.subplots(figsize=(10,5))
+    plot = sns.countplot(x=1, data=data)
+    plt.xlabel('Emotion category')
+    plt.ylabel('frequecy')
+    plt.title('Emotion Category vs Number of particular emotion data ')
+    plt.savefig(Dpath.ISEAR_VISUALIZE + 'Emotion_category_Counts.png')
+
 
     return target_labels, data
