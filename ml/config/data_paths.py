08508bf34aefd4efda5b3a0e4cfe2a0c922333be
bikashgup <bikashgup@hotmail.com>
2020-05-15 12:23:28 +0545

    Editted config file
    - added data_paths for storing confusion_matrix plots and classifcation
    reports

--- ml/config/data_paths.py
+++ ml/config/data_paths.py
@@ -2,5 +2,13 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
+### ISEAR
 
 ISEAR_DATA_PATH = BASE_DIR + '/ml/data/ISEAR.csv'
+
+ISEAR_VISUALIZE = BASE_DIR + '/ml/data_visualize/ISEAR/'
+
+ISEAR_OUPUTS  = BASE_DIR + '/ml/outputs/ISEAR/'
+
+ISEAR_CLASSIFICATION_REPORTS = ISEAR_OUPUTS + 'classification_reports/'
+ISEAR_CONFUSION_MATRIX = ISEAR_OUPUTS + 'confusion_matrix/'
