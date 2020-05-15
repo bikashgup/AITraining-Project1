fbe8250e20deee7786ba803372922752915d85b6
Aditi-Gajurel <aditigajurel1998@gmail.com>
2020-05-15 16:17:44 +0545

    Changes to baseline model

--- ml/preprocess_with_classifier/classifier.py
+++ ml/preprocess_with_classifier/classifier.py
@@ -1,23 +1,49 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import seaborn as sns
+import mlflow
+import mlflow.sklearn
+from urllib.parse import urlparse
+
 
 import load_data, dtm, evaluate, outputtxt
-
 
 df = load_data.data_load()
 X = dtm.doc_term_matrix(df)
 y = df['Emotion']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=42)
-
+top_words = 5000
 
 def train():
 
-    clf = SGDClassifier(loss='hinge', penalty='l1')
-    clf.fit(X_train, y_train)
+    with mlflow.start_run():
+
+        clf = SGDClassifier(loss='hinge', penalty='l2')
+
+        clf.fit(X_train, y_train)
     return clf
 
+
 def test(clf):
-    resultList = clf.predict(X_test)
+    resultList = clf.predict(X_train)
+    model = "SGD-L2"
     outputtxt.write2file("../Output_txtfile/bigram.output.txt", resultList)
-    evaluate.evaluation(resultList, y_test)
-    evaluate.plot_confusion_matrix(y_test, resultList)
+    accuracy = evaluate.evaluation(y_train, resultList)
+    confusion = evaluate.plot_confusion_matrix(y_train, resultList)
+    mlflow.log_param("accuracy", accuracy)
+    mlflow.log_param("Classifier", model)
+
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+    # Model registry does not work with file store
+    if tracking_url_type_store != "file":
+
+        # Register the model
+        # There are other ways to use the Model Registry, which depends on the use case,
+        # please refer to the doc for more information:
+        # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+        mlflow.sklearn.log_model(clf, "model", registered_model_name="SGD")
+    else:
+        mlflow.sklearn.log_model(clf, "model")
--- ml/preprocess_with_classifier/evaluate.py
+++ ml/preprocess_with_classifier/evaluate.py
@@ -11,8 +11,11 @@ def evaluation(y_actual, y_test):
     This evaluates the classifier with confusion matrix and accuracy.
 
     """
-    print("Confusion Matrix:\n", confusion_matrix(y_actual, y_test))
+    # print("Confusion Matrix:\n", confusion_matrix(y_actual, y_test))
+    accuracy = accuracy_score(y_actual, y_test)
+    confusion = confusion_matrix(y_actual, y_test)
     print("THe accuracy of SGD Classifier is: ", accuracy_score(y_actual, y_test))
+    return accuracy
 
 def plot_confusion_matrix(y_actual, y_pred):
   data = confusion_matrix(y_actual, y_pred)
@@ -25,3 +28,36 @@ def plot_confusion_matrix(y_actual, y_pred):
   sn.heatmap(df_cm, cmap = "Blues", ax =ax, annot =True, fmt = "d")
   ax.set_title('Confusion Matrix')
   plt.show()
+
+def plot_classification_report(cr, title='Classification report ', with_avg_total=False, cmap=plt.cm.Blues):
+
+    lines = cr.split('\n')
+
+    classes = []
+    plotMat = []
+    for line in lines[2 : (len(lines) - 3)]:
+        #print(line)
+        t = line.split()
+        # print(t)
+        classes.append(t[0])
+        v = [float(x) for x in t[1: len(t) - 1]]
+        print(v)
+        plotMat.append(v)
+
+    if with_avg_total:
+        aveTotal = lines[len(lines) - 1].split()
+        classes.append('avg/total')
+        vAveTotal = [float(x) for x in t[1:len(aveTotal) - 1]]
+        plotMat.append(vAveTotal)
+
+
+    plt.imshow(plotMat, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    x_tick_marks = np.arange(3)
+    y_tick_marks = np.arange(len(classes))
+    plt.xticks(x_tick_marks, ['precision', 'recall', 'f1-score'], rotation=45)
+    plt.yticks(y_tick_marks, classes)
+    plt.tight_layout()
+    plt.ylabel('Classes')
+    plt.xlabel('Measures')
