86f2a6c8eb2f94fbc7e0c1e498fbf4080a37efeb
bikashgup <bikashgup@hotmail.com>
2020-05-19 00:24:05 +0545

    Editted the file
    - added dependencies needed for project
        *  nlpaug==0.0.14
        * Flask==1.1.2
        * pymongo-3.10.1
        * MarkupSafe==1.1.1
        * Jinja2==2.11.2
        * gunicorn==20.0.4
       *  docker==4.2.0
        * click==7.1.2
        * Werkzeug==1.0.1
        * six==1.14.0
        * python-dateutil==2.8.1
        * python-dotenv==0.13.0
        * bson==0.5.9

--- requirements.py
+++ requirements.py
@@ -6,7 +6,18 @@ numpy==1.18.4
 matplotlib==3.2.1
 nltk==3.5
 mlflow==1.8.0
-
-
+nlpaug==0.0.14
+Flask==1.1.2
+pymongo-3.10.1
+MarkupSafe==1.1.1
+Jinja2==2.11.2
+gunicorn==20.0.4
+docker==4.2.0
+click==7.1.2
+Werkzeug==1.0.1
+six==1.14.0
+python-dateutil==2.8.1
+python-dotenv==0.13.0
+bson==0.5.9
 
 
