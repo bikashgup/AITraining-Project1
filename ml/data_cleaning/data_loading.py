8f70f6dd5538457f86f2861bb22b91e113a25adc
bikashgup <bikashgup@hotmail.com>
2020-05-19 03:23:40 +0545

    New data loading function  created
    -  for handling augmented dataset

--- ml/data_cleaning/data_loading.py
+++ ml/data_cleaning/data_loading.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from ..config import data_paths as Dpath
+from ...settings import BASE_DIR
+from ...utils import create_folder
 
 def loading_ISEAR_data(path):
     '''
@@ -19,31 +20,79 @@ def loading_ISEAR_data(path):
     '''
     print(path)
 
-    data = pd.read_csv(path, header=None)
+    data = pd.read_csv(path, names=['index', 'labels', 'sentences'])
     print(data.head())
 
-    data.drop(columns=[0], inplace=True)
-    target_labels = data[1].value_counts().keys().tolist()
+    data.drop(columns=['index'], inplace=True)
+    target_labels = data['labels'].value_counts().keys().tolist()
     print(target_labels)
 
-    # data[1] = data[1].astype('category')
+    data['labels'] = data['labels'].astype('category')
 
     ## adding the column containing numerical representatipn of emotion in column 1
 
-    # data[3] = data[1].cat.codes
+    data['label_id'] = data['labels'].cat.codes
 
     ## sorting the dataframe according to emotion id i.e column 1
 
-    # data = data.sort_values(3)
+    data = data.sort_values('label_id')
 
     ## countplot for emotion column i.e column 1 in dataframe
 
     # fig, ax1 = plt.subplots(figsize=(10,5))
-    # plot = sns.countplot(x=1, data=data)
+    # plot = sns.countplot(x='labels', data=data)
+    # plt.ylabel('frequecy')
+    # plt.title('Emotion Category vs Number of particular emotion data ')
+    # filepath = BASE_DIR + 'out/ISEAR/data_visualization/'
+    # create_folder.create_folder(filepath)
+    # plt.savefig( filepath + 'Emotion_category_Counts.png')
+
+
+    return target_labels, data   
+
+
+def loading_augmented_ISEAR_data(path):
+    '''
+        This function:
+            -loads the data
+
+        Parameters:
+        --------------
+        path: data_path
+
+        returns:
+        ----------------
+        data: pandas dataframe
+        target_labels: target_labels
+    '''
+    print(path)
+
+    data = pd.read_csv(path)
+    print(data.head())
+
+    target_labels = data['labels'].value_counts().keys().tolist()
+    print(target_labels)
+
+    data['labels'] = data['labels'].astype('category')
+
+    # adding the column containing numerical representatipn of emotion in column 1
+
+    data['label_id'] = data['labels'].cat.codes
+
+    # sorting the dataframe according to emotion id i.e column 1
+
+    data = data.sort_values('label_id')
+
+    # countplot for emotion column i.e column 1 in dataframe
+
+    # fig, ax1 = plt.subplots(figsize=(10,5))
+    # plot = sns.countplot(x='labels', data=data)
     # plt.xlabel('Emotion category')
     # plt.ylabel('frequecy')
     # plt.title('Emotion Category vs Number of particular emotion data ')
-    # plt.savefig(Dpath.ISEAR_VISUALIZE + 'Emotion_category_Counts.png')
+    # filepath = BASE_DIR + '/out/ISEAR/data_visualization/'
+    # create_folder.create_folder(filepath)
+    # plt.savefig( filepath + 'Augemented_Emotion_category_Counts.png')
 
 
     return target_labels, data
