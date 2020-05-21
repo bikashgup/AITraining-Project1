68602f6c716e44c28500221d5a46e7cd695f9fc0
bikashgup <bikashgup@hotmail.com>
2020-05-21 20:13:38 +0545

    [Fix error] Plot for labels confidence

--- app/ISEAR_app/routes.py
+++ app/ISEAR_app/routes.py
@@ -23,16 +23,16 @@ label_encoder = read_file(CHECKPOINT+ 'experiment2/label_encoder')
 vectorizer = read_file(CHECKPOINT + 'experiment2/vectorizer')
 metrics = read_file(CHECKPOINT + 'experiment2/metrics')
 
-@run_app.route('/')
-def index():
-    data = {
+    
+
+@run_app.route('/save_model_result', methods=['GET', 'POST'])
+def save_model_result():
+    if request.method == 'GET':
+        data = {
         'metrics':metrics,
         'model': model.__class__.__name__
     }
     return render_template('index.html', data=data)
-
-@run_app.route('/save_model_result', methods=['POST'])
-def save_model_result():
     if request.method == 'POST':
         precision = request.form['precision']
         accuracy = request.form['accuracy']
@@ -55,7 +55,7 @@ def save_model_result():
         
         return render_template('index.html', data = {'metrics':metrics, 'model':model_name, 'success_text':'Your model saved succefuuly.'} )
     
-@run_app.route('/predict', methods=['GET', 'POST'])
+@run_app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'GET':
         return render_template('predict.html', data={'model':model.__class__.__name__})
@@ -64,28 +64,29 @@ def predict():
         text = data
         data = data.lower()
         data = vectorizer.transform([data,])
-        print(data)
         y = model.predict(data)
         proba = model.predict_proba(data)
         
         predicted_value = label_encoder.inverse_transform(y)
         classes = label_encoder.classes_
         y_pos = np.arange(len(classes))
-        print(classes, predicted_value, proba)
         plt.figure(figsize = (10, 8))
         plt.barh(y_pos, proba[0], align='center')
         plt.yticks(y_pos, tuple(classes))
         plt.xlabel('predict_proba')
+        filepath = BASE_DIR + '/static/images/'
+        filename = predicted_value[0] +'_'+str(dt.now().time().second)+'.png'
+        print(filename)
+        plt.savefig(filepath +filename)
         data = {
             'model':model.__class__.__name__,
             'predicted_value': predicted_value[0],
-            'filepath': '/images/image.png',
+            'filepath': '/images/' + filename,
             'datetime': dt.now()
         }
         database = shared_components['db']
         database.predicted_output.insert(data)
-        filepath = BASE_DIR + '/static/images/'
-        plt.savefig(filepath +'image.png')
+        
         data['success_text'] = 'predictted your output'
         data['text'] = text
 
